15feb1baacb875316d35b59bf1db196b779d72f8
André <andrev.rebonato@gmail.com>
2024-11-28 00:17:27 -0300

    progress: started tests

---
+++ dot/parachain/prospective-parachains/prospective_parachains_test.go
@@ -0,0 +1,134 @@
+package prospectiveparachains
+
+import (
+	"testing"
+
+	parachaintypes "github.com/ChainSafe/gossamer/dot/parachain/types"
+	"github.com/ChainSafe/gossamer/lib/common"
+)
+
+const MAX_POV_SIZE = 1_000_000
+
+func dummyPVD(parentHead parachaintypes.HeadData, relayParentNumber uint32) parachaintypes.PersistedValidationData {
+	return parachaintypes.PersistedValidationData{
+		ParentHead:             parentHead,
+		RelayParentNumber:      relayParentNumber,
+		RelayParentStorageRoot: common.EmptyHash,
+		MaxPovSize:             MAX_POV_SIZE,
+	}
+}
+
+func dummyCandidateReceiptBadSig(
+	relayParentHash common.Hash,
+	commitments *common.Hash,
+) parachaintypes.CandidateReceipt {
+	var commitmentsHash common.Hash
+
+	if commitments != nil {
+		commitmentsHash = *commitments
+	} else {
+		// Handle the case where commitments is nil
+		// might want to use a default hash or the relayParentHash
+		commitmentsHash = relayParentHash
+	}
+
+	return parachaintypes.CandidateReceipt{
+		CommitmentsHash: commitmentsHash,
+	}
+}
+
+func MakeCandidate(
+	relayParent common.Hash,
+	relayParentNumber uint32,
+	paraID parachaintypes.ParaID,
+	parentHead parachaintypes.HeadData,
+	headData parachaintypes.HeadData,
+	validationCodeHash parachaintypes.ValidationCodeHash,
+) parachaintypes.CommittedCandidateReceipt {
+	pvd := dummyPVD(parentHead, relayParentNumber)
+
+	commitments := parachaintypes.CandidateCommitments{
+		HeadData:                  headData,
+		HorizontalMessages:        []parachaintypes.OutboundHrmpMessage{},
+		UpwardMessages:            []parachaintypes.UpwardMessage{},
+		NewValidationCode:         nil,
+		ProcessedDownwardMessages: 0,
+		HrmpWatermark:             relayParentNumber,
+	}
+
+	commitmentsHash := commitments.Hash()
+
+	candidate := dummyCandidateReceiptBadSig(relayParent, &commitmentsHash)
+	candidate.CommitmentsHash = commitments.Hash()
+	candidate.Descriptor.ParaID = paraID
+
+	pvdh, err := pvd.Hash()
+
+	if err != nil {
+		panic(err)
+	}
+
+	candidate.Descriptor.PersistedValidationDataHash = pvdh
+	candidate.Descriptor.ValidationCodeHash = validationCodeHash
+
+	result := parachaintypes.CommittedCandidateReceipt{
+		Descriptor:  candidate.Descriptor,
+		Commitments: commitments,
+	}
+
+	return result
+}
+
+func introduceSecondedCandidate(
+	candidate parachaintypes.CommittedCandidateReceipt,
+	pvd parachaintypes.PersistedValidationData,
+) {
+	req := IntroduceSecondedCandidateRequest{
+		CandidateParaID:         candidate.Descriptor.ParaID,
+		CandidateReceipt:        candidate,
+		PersistedValidationData: pvd,
+	}
+
+	response := make(chan bool)
+
+	msg := IntroduceSecondedCandidate{
+		IntroduceSecondedCandidateRequest: req,
+	}
+
+	msg.Response = response
+
+	<-response
+}
+
+func introduceSecondedCandidateFailed(
+	candidate parachaintypes.CommittedCandidateReceipt,
+	pvd parachaintypes.PersistedValidationData,
+) {
+	req := IntroduceSecondedCandidateRequest{
+		CandidateParaID:         candidate.Descriptor.ParaID,
+		CandidateReceipt:        candidate,
+		PersistedValidationData: pvd,
+	}
+
+	response := make(chan bool)
+
+	msg := IntroduceSecondedCandidate{
+		IntroduceSecondedCandidateRequest: req,
+	}
+
+	msg.Response = response
+
+	response <- false
+}
+
+func TestHandleIntroduceSecondedCandidate(
+	t *testing.T,
+) {
+	candidateRelayParent := common.Hash{0x01}
+	paraId := parachaintypes.ParaID(1)
+	parentHead := parachaintypes.HeadData{}
+	headData := parachaintypes.HeadData{}
+	validationCodeHash := parachaintypes.ValidationCodeHash{0x01}
+	candidateRelayParentNumber := uint32(1)
+
+}
